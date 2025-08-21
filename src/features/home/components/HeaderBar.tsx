70f660a0ba588bfbd462f884c81fd72e176a18ed
als8921 <als8921@naver.com>
2025-08-21 17:17:51 +0900

    chore: 홈 헤더 로고 이미지 변경

--- src/features/home/components/HeaderBar.tsx
+++ src/features/home/components/HeaderBar.tsx
@@ -9,17 +9,11 @@ const HeaderBar: React.FC = () => {
         <View className="flex-row items-center">
           <Image
             source={require("../../../assets/images/logo.png")}
-            className="w-14 h-14 -mr-1.5"
             resizeMode="contain"
+            className="h-12 w-32 ml-2"
           />
-          <Text
-            className="text-3xl font-extrabold"
-            style={{ color: "#0F0D85", letterSpacing: -1.5 }}
-          >
-            시장보장
-          </Text>
         </View>
-        <Ionicons name="notifications-outline" size={24} color="#6b7280" />
+        <Ionicons name="notifications-outline" size={24} color="#0F0D85" />
       </View>
     </View>
   );
